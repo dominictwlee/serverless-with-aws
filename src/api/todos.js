9f69d06e65b4b5ff12a79d398b9518950001e8ca
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-06-24 13:15:43 +0100

    done

--- src/api/todos.js
+++ src/api/todos.js
@@ -5,6 +5,6 @@ module.exports.handler = (event, ctx, done) => {
   ]
   done(null, {
     statusCode: 200,
-    body: JSON.stringify({data: todos})
+    body: JSON.stringify({data: todos, query: event.queryStringParameters})
   })
 }
